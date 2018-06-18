df638750f38ebb865189008f53235045f441dab0
Layth <laythq@gmail.com>
2018-06-18 16:50:38 +0100

    Add method #top_up, max_balance constant and max_balance guard

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,6 +1,15 @@
 class Oystercard
+
   attr_reader :balance
+  MAX_BALANCE = 90
+
   def initialize
     @balance = 0
   end
+
+  def top_up(amount)
+    fail "Maximum limit of #{MAX_BALANCE} exceeded." if balance + amount > MAX_BALANCE
+    @balance += amount
+  end
+
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -6,4 +6,19 @@ describe Oystercard do
       expect(Oystercard.new.balance).to eq(0)
     end
   end
+
+  describe '#top_up()' do
+    it 'Tops up the balance with method argument' do
+      expect{subject.top_up 10}. to change{subject.balance}.by 10
+    end
+
+    it 'Raises an error if limit is exceeded' do
+      max = Oystercard::MAX_BALANCE
+      subject.top_up(max)
+      expect{subject.top_up(1)}.to raise_error("Maximum limit of #{max} exceeded.")
+    end
+
+
+  end
+
 end
