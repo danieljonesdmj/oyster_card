b0fa9af941185a551c0af24a3698b0d3a266b47c
danieljonesdmj <danieljonesdmj@gmail.com>
2018-06-19 15:50:34 +0100

    card remembers entry station

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,6 +1,7 @@
 require 'oystercard'
 
 describe Oystercard do
+  let(:station) { double :station }
   describe '#balance' do
     it 'Starts with a balance of zero' do
       expect(Oystercard.new.balance).to eq(0)
@@ -27,26 +28,34 @@ describe Oystercard do
     describe '#touch_in' do
       it 'Changes in_journey to true' do
         subject.instance_variable_set(:@balance, Oystercard::MIN_BALANCE)
-        subject.touch_in
+        subject.touch_in(station)
         expect(subject.in_journey?).to eq true
       end
 
       it 'touch_in without minimum balance raises error' do
-        expect {subject.touch_in}.to raise_error "Insufficient Funds"
+        expect {subject.touch_in(station)}.to raise_error "Insufficient Funds"
+      end
+
+      it { is_expected.to respond_to(:touch_in).with(1).argument }
+
+
+      it 'should remember entry station after touch_in' do
+        subject.top_up(Oystercard::MIN_BALANCE)
+        expect { subject.touch_in(station) }. to change { subject.entry_station }.to station
       end
     end
 
     describe '#touch_out' do
-      it 'Changes in_journey to false' do
+      it 'Changes entry_station to nil' do
         subject.instance_variable_set(:@balance, Oystercard::MIN_BALANCE)
-        subject.touch_in
+        subject.touch_in(station)
         subject.touch_out
-        expect(subject.in_journey?).to eq false
+        expect(subject.entry_station).to eq nil
       end
 
       it 'Deducts the minimum fare when #touch_out is called' do
         subject.instance_variable_set(:@balance, Oystercard::MIN_BALANCE)
-        subject.touch_in
+        subject.touch_in(station)
         expect { subject.touch_out }. to change { subject.balance }.by(-Oystercard::MIN_BALANCE)
       end
     end
