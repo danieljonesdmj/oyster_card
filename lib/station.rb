0b0b3a7f06596ae47d8bf8514cabe0f03e69f75b
danieljonesdmj <danieljonesdmj@gmail.com>
2018-06-20 15:23:43 +0100

    added tests to check that station knows its name and zone

--- lib/station.rb
+++ lib/station.rb
@@ -1,2 +1,8 @@
 class Station
+  def initialize(name, zone)
+    @name = name
+    @zone = zone
+  end
+  attr_reader :name
+  attr_reader :zone
 end
--- spec/station_spec.rb
+++ spec/station_spec.rb
@@ -1,5 +1,14 @@
 require 'station'
 
 describe Station do
+  let(:station) { Station.new('Oxford St', 'zone_1') }
+  describe 'return what zone a station is in' do
+    it 'knows what it is name is' do
+      expect(station.name).to eq 'Oxford St'
+    end
 
+    it 'knows what zone it is in' do
+      expect(station.zone).to eq 'zone_1'
+    end
+  end
 end
